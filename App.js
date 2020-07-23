7568a30010513db1f2941c4b2a27776f61332b0c
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-07-23 21:52:21 +0300

    AddTodo

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import {Navbar} from './src/Navbar'
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const newTodo = {
+    id: Date.now()
+  }
+
   return (
-    <View style={styles.container}>
-      <Navbar />
+    <View>
+      <Navbar title='My Buy' />
+      <View style={styles.container}>
+        <AddTodo />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
 });
